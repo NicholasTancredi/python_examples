ed7926cf81951c82dcef2030303c138639637d93
Nicholas Tancredi <nicholastancredi@gmail.com>
2018-11-30 09:03:49 -0400

    fixed complex example

--- examples/complex_pydantic.py
+++ examples/complex_pydantic.py
@@ -1,15 +1,13 @@
 from enum import Enum
-# from pydantic import BaseModel
-# from typing import NamedTuple
-from pytypes import typechecked
-import arrow
+from typing import NamedTuple, Optional
 
-# import project_constants as pc
-# from src.pose_platform_constants.status import StatusEnum, StatusTypes
-# from src.pose_platform_constants.assessments import AssessmentEnum
+import arrow
+from pydantic import BaseModel, PydanticValueError
+from pytypes import typechecked
+
 
 class KindEnum(Enum):
-    pose_job = 'pose_job'
+    POSEJOB = 'POSEJOB'
 
 
 @typechecked
@@ -18,23 +16,15 @@ class BlobInfoObject(NamedTuple):
     bucket: str = 'pc.BUCKET_NAME'
 
 
-from pydantic import BaseModel, PydanticValueError
-from typing import NamedTuple, Optional
-# ...
 class PoseJobIdKeyError(PydanticValueError):
     code = 'PoseJobIdKeyError'
     msg_template = '"id" is required when "outputBlobInfoVideo" is not provided.'
 
 
 class PoseJob(BaseModel):
-    # REQUIRED
-    # assessment: AssessmentEnum
-    # inputBlobInfo: BlobInfoObject
-
     # DEFAULTS
     modelVersion: str = '2.0.2'
-    # status: StatusEnum = StatusEnum.INITIALIZING
-    # kind: KindEnum = KindEnum.pose_job
+    kind: KindEnum = KindEnum.POSEJOB
     createdAt: str = arrow.utcnow().format()
     updatedAt: str = arrow.utcnow().format()
 
@@ -54,23 +44,10 @@ class PoseJob(BaseModel):
         # typechecked before it gets to outputBlobInfoVideo
         super().__init__(**_kwargs)
 
-        # NOTE: Default bucket can be set on BlobInfoObject
-        # defaultBucket = pc.BUCKET_NAME
-        # ...
-        # @typechecked
-        # class BlobInfoObject(NamedTuple):
-        #     name: str
-        #     bucket: str = pc.BUCKET_NAME
         if 'outputBlobInfoVideo' not in _kwargs:
             #  Probably shouldn't even have this happen, but I'm assuming other things would need to be restructured...
             if _kwargs['id'] is None:
                 raise PoseJobIdKeyError()
-
-            # These would throw an error `value is not a valid tuple (type=type_error.tuple)`
-            # _kwargs['outputBlobInfoVideo'] = {
-            #     'name': '{}/video.mp4'.format(_kwargs['id']),
-            #     'bucket': defaultBucket
-            # }
 
             _kwargs['outputBlobInfoPoseData'] = BlobInfoObject(
                 name='{}/video.mp4'.format(_kwargs['id'])
@@ -85,10 +62,3 @@ class PoseJob(BaseModel):
             )
 
         super().__init__(**_kwargs)
-
-
-class Test(BaseModel):
-    a: str = 'a'
-
-
-Test()
